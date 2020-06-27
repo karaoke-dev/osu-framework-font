17cc09a44eb1962a455074c8f943169b4712663e
andy840119 <andy840119@gmail.com>
2020-06-27 17:31:14 +0900

    Add test project file.

---
+++ osu.Karaoke.Font.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace osu.Karaoke.Font
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
