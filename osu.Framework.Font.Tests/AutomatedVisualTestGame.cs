1288ae3d6bd639d2c5130e244b10b376e74f7129
andy840119 <andy840119@gmail.com>
2020-06-27 17:57:32 +0900

    Initial test project

---
+++ osu.Framework.Font.Tests/AutomatedVisualTestGame.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Testing;
+
+namespace osu.Framework.Tests
+{
+    internal class AutomatedVisualTestGame : TestGame
+    {
+        public AutomatedVisualTestGame()
+        {
+            Add(new TestBrowserTestRunner(new TestBrowser()));
+        }
+    }
+}
---
+++ osu.Framework.Font.Tests/VisualTestGame.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Platform;
+using osu.Framework.Testing;
+
+namespace osu.Framework.Tests
+{
+    internal class VisualTestGame : TestGame
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Child = new DrawSizePreservingFillContainer
+            {
+                Children = new Drawable[]
+                {
+                    new TestBrowser(),
+                    new CursorContainer(),
+                },
+            };
+        }
+
+        public override void SetHost(GameHost host)
+        {
+            base.SetHost(host);
+            host.Window.CursorState |= CursorState.Hidden;
+        }
+    }
+}
