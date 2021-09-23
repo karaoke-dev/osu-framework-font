d973f3417b20f7513b9a6e2ce3c1c3efbfb97717
andy840119 <andy840119@gmail.com>
2021-09-24 02:22:22 +0900

    Implement customzied shader effect to let user able to apply the value.

---
+++ osu.Framework.Font/Graphics/Shaders/CustomizedShader.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Graphics.OpenGL.Buffers;
+
+namespace osu.Framework.Graphics.Shaders
+{
+    /// <summary>
+    /// Shader with customized property
+    /// </summary>
+    public abstract class CustomizedShader : ICustomizedShader
+    {
+        private readonly IShader shader;
+
+        protected CustomizedShader(IShader originShader)
+        {
+            shader = originShader;
+        }
+
+        public void Bind() => shader.Bind();
+
+        public void Unbind() => shader.Unbind();
+
+        public Uniform<T> GetUniform<T>(string name) where T : struct, IEquatable<T>
+            => shader.GetUniform<T>(name);
+
+        public bool IsLoaded => shader.IsLoaded;
+
+        public abstract void ApplyValue(FrameBuffer current);
+    }
+}
---
+++ osu.Framework.Font/Graphics/Shaders/ICustomizedShader.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.OpenGL.Buffers;
+
+namespace osu.Framework.Graphics.Shaders
+{
+    public interface ICustomizedShader : IShader
+    {
+        void ApplyValue(FrameBuffer current);
+    }
+}
