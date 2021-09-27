f70c744cdcdcff963e2b3f7188fb84907d0bb605
andy840119 <andy840119@gmail.com>
2021-09-28 01:27:09 +0900

    Refactor test case.

--- osu.Framework.Font.Tests/Visual/Sprites/TestSceneKaraokeSpriteTextWithShader.cs
+++ osu.Framework.Font.Tests/Visual/Sprites/TestSceneKaraokeSpriteTextWithShader.cs
@@ -4,149 +4,112 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
-using osu.Framework.Allocation;
-using osu.Framework.Font.Tests.Helper;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
+using osu.Framework.Font.Tests.Helper;
 using osu.Framework.Graphics.Shaders;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Testing;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Framework.Font.Tests.Visual.Sprites
 {
-    public class TestSceneKaraokeSpriteTextWithShader : TestScene
+    public class TestSceneKaraokeSpriteTextWithShader : BackgroundGridTestSample
     {
-        [Resolved]
-        private ShaderManager shaderManager { get; set; }
+        private readonly KaraokeSpriteText karaokeSpriteText;
 
-        [TestCase("CRT")]
-        [TestCase("not_found_shader")] // notice that missing shader will let whole sprite text being white.
-        public void ApplyShader(string shaderName)
+        public TestSceneKaraokeSpriteTextWithShader()
         {
-            var shader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, shaderName);
-            AddStep("Create lyric", () => setContents(spriteText =>
+            Child = karaokeSpriteText = new KaraokeSpriteText
             {
-                spriteText.LeftTextColour = Color4.Green;
-                spriteText.RightTextColour = Color4.Red;
-                spriteText.Shaders = new[]
-                {
-                    shader,
-                };
-            }));
-        }
-
-        [TestCase("CRT", "CRT")]
-        [TestCase("CRT", "not_found_shader")] // notice that missing shader will let whole sprite text being white.
-        public void ApplyLyricTextShader(string leftShaderName, string rightShaderName)
-        {
-            var leftShader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, leftShaderName);
-            var rightShader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, rightShaderName);
-            AddStep("Create lyric", () => setContents(spriteText =>
-            {
-                spriteText.LeftTextColour = Color4.Green;
-                spriteText.RightTextColour = Color4.Red;
-                spriteText.LeftLyricTextShaders = new[]
-                {
-                    leftShader,
-                };
-                spriteText.RightLyricTextShaders = new[]
-                {
-                    rightShader,
-                };
-            }));
-        }
-
-        [TestCase]
-        public void ApplyLyricTextShaderWithParams()
-        {
-            var shader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, OutlineShader.SHADER_NAME);
-            AddStep("Create lyric", () => setContents(spriteText =>
-            {
-                spriteText.LeftLyricTextShaders = new[]
-                {
-                    new OutlineShader(shader)
-                    {
-                        Radius = 10,
-                        OutlineColour = Color4.Green,
-                    },
-                };
-                spriteText.RightLyricTextShaders = new[]
-                {
-                    new OutlineShader(shader)
-                    {
-                        Radius = 10,
-                        OutlineColour = Color4.Red,
-                    },
-                };
-            }));
-        }
-
-        [TestCase]
-        public void ApplyShaderInBothPart()
-        {
-            var outlineShader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, OutlineShader.SHADER_NAME);
-            var crtShader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, "CRT");
-            AddStep("Create lyric", () => setContents(spriteText =>
-            {
-                // apply shader in karaoke sprite text.
-                spriteText.Shaders = new[]
-                {
-                    crtShader
-                };
-
-                // apply shader in lyric sprite text.
-                spriteText.LeftLyricTextShaders = new[]
-                {
-                    new OutlineShader(outlineShader)
-                    {
-                        Radius = 10,
-                        OutlineColour = Color4.Green,
-                    },
-                };
-                spriteText.RightLyricTextShaders = new[]
-                {
-                    new OutlineShader(outlineShader)
-                    {
-                        Radius = 10,
-                        OutlineColour = Color4.Red,
-                    },
-                };
-            }));
-        }
-
-        private void setContents(Action<KaraokeSpriteText> applySpriteTextProperty)
-        {
-            var startTime = Time.Current;
-            Child = new Container
-            {
-                RelativeSizeAxes = Axes.Both,
-                Children = new Drawable[]
-                {
-                    new Box
-                    {
-                        Colour = Color4.CornflowerBlue,
-                        RelativeSizeAxes = Axes.Both,
-                    },
-                    new KaraokeSpriteText
-                    {
-                        Text = "カラオケ！",
-                        Rubies = TestCaseTagHelper.ParseParsePositionTexts(new[] { "[0,1]:か", "[1,2]:ら", "[2,3]:お", "[3,4]:け" }),
-                        Romajies = TestCaseTagHelper.ParseParsePositionTexts(new[] { "[0,1]:ka", "[1,2]:ra", "[2,3]:o", "[3,4]:ke" }),
-                        TimeTags = new Dictionary<TextIndex, double>
-                        {
-                            { new TextIndex(0), startTime + 500 },
-                            { new TextIndex(1), startTime + 600 },
-                            { new TextIndex(2), startTime + 1000 },
-                            { new TextIndex(3), startTime + 1500 },
-                            { new TextIndex(4), startTime + 2000 },
-                        },
-                        Scale = new Vector2(5)
-                    }.With(applySpriteTextProperty),
-                }
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Text = "カラオケ！",
+                Rubies = TestCaseTagHelper.ParseParsePositionTexts(new[] { "[0,1]:か", "[1,2]:ら", "[2,3]:お", "[3,4]:け" }),
+                Romajies = TestCaseTagHelper.ParseParsePositionTexts(new[] { "[0,1]:ka", "[1,2]:ra", "[2,3]:o", "[3,4]:ke" }),
+                Scale = new Vector2(2),
+                LeftTextColour = Color4.Green,
+                RightTextColour = Color4.Red,
             };
+        }
+
+        [Test]
+        public void ApplyShader()
+        {
+            AddStep("Apply CRT shader", () => karaokeSpriteText.Shaders = new[]
+            {
+                GetShader("CRT"),
+            });
+
+            AddStep("Clear shader", () => karaokeSpriteText.Shaders = null);
+        }
+
+        [Test]
+        public void ApplyLeftLyricTextShader()
+        {
+            var outlineShader = GetShader(OutlineShader.SHADER_NAME);
+            AddStep("Apply Outline shader in left text", () => karaokeSpriteText.LeftLyricTextShaders = new[]
+            {
+                new OutlineShader(outlineShader)
+                {
+                    Radius = 10,
+                    OutlineColour = Color4.Green,
+                },
+            });
+
+            AddStep("Clear shader from left text", () => karaokeSpriteText.LeftLyricTextShaders = null);
+        }
+
+        [Test]
+        public void ApplyRightLyricTextShader()
+        {
+            var outlineShader = GetShader(OutlineShader.SHADER_NAME);
+            var rainbowShader = GetShader(RainbowShader.SHADER_NAME);
+
+            AddStep("Apply Outline shader in right text", () => karaokeSpriteText.RightLyricTextShaders = new IShader[]
+            {
+                new OutlineShader(outlineShader)
+                {
+                    Radius = 1,
+                    OutlineColour = Color4.Blue,
+                },
+                new StepShader
+                {
+                    Name = "Outline with rainbow effect",
+                    StepShaders = new IShader[]
+                    {
+                        new OutlineShader(outlineShader)
+                        {
+                            Radius = 10,
+                            OutlineColour = Color4.White,
+                        },
+                        new RainbowShader(rainbowShader)
+                    }
+                },
+            });
+
+            AddStep("Clear shader from right text", () => karaokeSpriteText.RightLyricTextShaders = null);
+        }
+
+        protected new void AddStep(string description, Action action)
+        {
+            base.AddStep(description, () =>
+            {
+                action?.Invoke();
+                resetTime();
+            });
+
+            void resetTime()
+            {
+                var startTime = Time.Current;
+                karaokeSpriteText.TimeTags = new Dictionary<TextIndex, double>
+                {
+                    { new TextIndex(0), startTime + 500 },
+                    { new TextIndex(1), startTime + 600 },
+                    { new TextIndex(2), startTime + 1000 },
+                    { new TextIndex(3), startTime + 1500 },
+                    { new TextIndex(4), startTime + 2000 },
+                };
+            }
         }
     }
 }
