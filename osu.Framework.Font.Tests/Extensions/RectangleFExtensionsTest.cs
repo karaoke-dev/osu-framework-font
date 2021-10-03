d2adc3c6f87f7f491bc80300314d73a8e3843e4c
andy840119 <andy840119@gmail.com>
2021-10-03 16:13:49 +0900

    Implement scale rectangle.

---
+++ osu.Framework.Font.Tests/Extensions/RectangleFExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Extensions;
+using osu.Framework.Graphics.Primitives;
+
+namespace osu.Framework.Font.Tests.Extensions
+{
+    public class RectangleFExtensionsTest
+    {
+        [TestCase(2, Anchor.Centre, 0, 5, 40, 20)]
+        [TestCase(2, Anchor.TopLeft, 10, 10, 40, 20)]
+        [TestCase(2, Anchor.TopRight, -10, 10, 40, 20)]
+        [TestCase(2, Anchor.BottomLeft, 10, 0, 40, 20)]
+        [TestCase(2, Anchor.BottomRight, -10, 0, 40, 20)]
+        public void TestScale(float scale, Anchor origin, float x, float y, float width, float height)
+        {
+            var rectangle = new RectangleF(10, 10, 20, 10);
+            var target = rectangle.Scale(scale, origin);
+
+            Assert.AreEqual(target.X, x);
+            Assert.AreEqual(target.Y, y);
+            Assert.AreEqual(target.Width, width);
+            Assert.AreEqual(target.Height, height);
+        }
+    }
+}
---
+++ osu.Framework.Font/Graphics/Extensions/RectangleFExtensions.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Extensions.EnumExtensions;
+using osu.Framework.Graphics.Primitives;
+using osuTK;
+
+namespace osu.Framework.Graphics.Extensions
+{
+    public static class RectangleFExtensions
+    {
+        public static RectangleF Scale(this RectangleF source, float scale, Anchor origin = Anchor.Centre)
+            => source.Scale(new Vector2(scale), origin);
+
+        public static RectangleF Scale(this RectangleF source, Vector2 scale, Anchor origin = Anchor.Centre)
+        {
+            var newWidth = source.Width * scale.X;
+            var newHeight = source.Height * scale.Y;
+
+            var x = source.X - getXScale(origin) * (newWidth - source.Width);
+            var y = source.Y - getYScale(origin) * (newHeight - source.Height);
+
+            return new RectangleF(x, y, newWidth, newHeight);
+
+            static float getXScale(Anchor origin)
+            {
+                if (origin.HasFlagFast(Anchor.x0))
+                    return 0;
+
+                if (origin.HasFlagFast(Anchor.x1))
+                    return 0.5f;
+
+                if (origin.HasFlagFast(Anchor.x2))
+                    return 1f;
+
+                return 100;
+            }
+
+            static float getYScale(Anchor origin)
+            {
+                if (origin.HasFlagFast(Anchor.y0))
+                    return 0;
+
+                if (origin.HasFlagFast(Anchor.y1))
+                    return 0.5f;
+
+                if (origin.HasFlagFast(Anchor.y2))
+                    return 1f;
+
+                return 100;
+            }
+        }
+    }
+}
