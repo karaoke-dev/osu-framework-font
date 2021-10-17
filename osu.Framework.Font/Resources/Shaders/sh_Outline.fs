8d2add78cc2b4f7b35628c9a5cb6dcab4517477c
andy840119 <andy840119@gmail.com>
2021-10-17 16:43:36 +0800

    adjust shader code

--- osu.Framework.Font/Resources/Shaders/sh_Outline.fs
+++ osu.Framework.Font/Resources/Shaders/sh_Outline.fs
@@ -12,20 +12,16 @@ lowp vec4 outline(sampler2D tex, int radius, mediump vec2 texCoord, mediump vec2
 {
 	mediump vec4 sum = texture2D(tex, texCoord);
 
-	for (int size = 2; size <= 200; size += 2)
+	for (int size = 2; size <= radius; size += 2)
 	{
 		// draw the circle outline with target size
 		for(int degree = 0; degree < 360; degree += 5)
 		{
-			mediump float xx = sin(degree);
-			mediump float yy = cos(degree);
+			mediump vec2 offset = vec2(sin(degree), cos(degree)) * size / texSize;
 
 			// create sample with target position.
-			mediump vec4 texture = texture2D(tex, texCoord + vec2( xx, yy ) * size / texSize).a * colour;
-			sum += texture;
+			sum += texture2D(tex, texCoord + offset).a * colour;
 		}
-		if (size >= radius)
-			break;
 	}
 
 	// draw origin texture2D in center
