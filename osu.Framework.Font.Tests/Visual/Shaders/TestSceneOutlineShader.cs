eb9442c3a68c4b15941fb7a5b44c19aae074a34c
andy840119 <andy840119@gmail.com>
2021-10-31 10:55:25 +0800

    pre-calculate all the position for preventing calculate in each frame.

--- osu.Framework.Font.Tests/Visual/Shaders/TestSceneOutlineShader.cs
+++ osu.Framework.Font.Tests/Visual/Shaders/TestSceneOutlineShader.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using NUnit.Framework;
 using osu.Framework.Extensions;
 using osu.Framework.Extensions.Color4Extensions;
@@ -27,6 +28,26 @@ namespace osu.Framework.Font.Tests.Visual.Shaders
                     })
                 };
             });
+        }
+
+        [TestCase(128)]
+        public void CreateAnglePositionParams(int samples)
+        {
+            // it's a script on creating const params in shader.
+
+            const float pi = 3.14159265359f;
+            double angle = 0.0f;
+
+            Console.WriteLine($"const lowp vec2 angelPosition[{samples}] = lowp vec2[](");
+
+            for (int i = 0; i < samples; i++)
+            {
+                var last = i == samples - 1;
+                angle += 1.0 / (samples / 2.0) * pi;
+                Console.WriteLine($"    lowp vec2({Math.Sin(angle):N2}, {Math.Cos(angle):N2})" + (last ? "" : ","));
+            }
+
+            Console.WriteLine(");");
         }
     }
 }
