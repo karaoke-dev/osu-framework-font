75fe07b75ffcd1456b1f0b9a8bf55508c9e129d5
為什麼 <andy840119@gmail.com>
2021-11-11 09:45:15 +0800

    new can use easy way to create instance.

--- osu.Framework.Font/Extensions/ShaderManagerExtensions.cs
+++ osu.Framework.Font/Extensions/ShaderManagerExtensions.cs
@@ -1,18 +1,18 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Graphics.Shaders;
 
 namespace osu.Framework.Extensions
 {
     public static class ShaderManagerExtensions
     {
-        public static T LocalInternalShader<T>(this ShaderManager shaderManager) where T : InternalShader
+        public static T LocalInternalShader<T>(this ShaderManager shaderManager) where T : InternalShader, new()
         {
-            var shaderName = ((T)Activator.CreateInstance(typeof(T), default(IShader))).ShaderName;
-            var shader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, shaderName);
-            return (T)Activator.CreateInstance(typeof(T), shader);
+            var internalShader = new T();
+            var shader = shaderManager.Load(VertexShaderDescriptor.TEXTURE_2, internalShader.ShaderName);
+            internalShader.AttachOriginShader(shader);
+            return internalShader;
         }
     }
 }
