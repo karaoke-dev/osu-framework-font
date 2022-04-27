d0ac97c5edcb78af780daa6dfa3117d83b97731e
andy840119 <andy840119@gmail.com>
2022-04-27 19:57:51 +0800

    Remove the un-need parts. Will add another test cases for time-tags.

--- osu.Framework.Font.Tests/Visual/Sprites/TestSceneKaraokeSpriteText.cs
+++ osu.Framework.Font.Tests/Visual/Sprites/TestSceneKaraokeSpriteText.cs
@@ -31,7 +31,7 @@ namespace osu.Framework.Font.Tests.Visual.Sprites
 
         [TestCase(false)]
         [TestCase(true)]
-        public void TestKaraokeSpriteTextTimeTag(bool manualTime)
+        public void TestKaraokeSpriteTextClock(bool manualTime)
         {
             if (manualTime)
             {
@@ -41,7 +41,7 @@ namespace osu.Framework.Font.Tests.Visual.Sprites
                 });
             }
 
-            AddStep("Default time tag", () =>
+            AddStep("Apply time-tags", () =>
             {
                 var startTime = getStartTime();
 
@@ -54,77 +54,6 @@ namespace osu.Framework.Font.Tests.Visual.Sprites
                     { new TextIndex(3), startTime + 1500 },
                     { new TextIndex(4), startTime + 2000 },
                 };
-            });
-            AddStep("Time tag with end state", () =>
-            {
-                var startTime = getStartTime();
-
-                karaokeSpriteText.Clock = manualTime ? new FramedClock(manualClock) : Clock;
-                karaokeSpriteText.TimeTags = new Dictionary<TextIndex, double>
-                {
-                    // カ
-                    { new TextIndex(0), startTime + 0 },
-                    { new TextIndex(0, TextIndex.IndexState.End), startTime + 100 },
-                    // ラ
-                    { new TextIndex(1), startTime + 1000 },
-                    { new TextIndex(1, TextIndex.IndexState.End), startTime + 1100 },
-                    // オ
-                    { new TextIndex(2), startTime + 2000 },
-                    { new TextIndex(2, TextIndex.IndexState.End), startTime + 2100 },
-                    // ケ
-                    { new TextIndex(3), startTime + 3000 },
-                    { new TextIndex(3, TextIndex.IndexState.End), startTime + 3100 },
-                    // !
-                    { new TextIndex(4), startTime + 4000 },
-                    { new TextIndex(4, TextIndex.IndexState.End), startTime + 4100 },
-                };
-            });
-            AddStep("Time tag with wrong order", () =>
-            {
-                var startTime = getStartTime();
-
-                karaokeSpriteText.Clock = manualTime ? new FramedClock(manualClock) : Clock;
-                karaokeSpriteText.TimeTags = new Dictionary<TextIndex, double>
-                {
-                    { new TextIndex(4), startTime + 2000 },
-                    { new TextIndex(3), startTime + 1500 },
-                    { new TextIndex(2), startTime + 1000 },
-                    { new TextIndex(1), startTime + 600 },
-                    { new TextIndex(0), startTime + 500 },
-                };
-            });
-            AddStep("Time tag with out of range", () =>
-            {
-                var startTime = getStartTime();
-
-                karaokeSpriteText.Clock = manualTime ? new FramedClock(manualClock) : Clock;
-                karaokeSpriteText.TimeTags = new Dictionary<TextIndex, double>
-                {
-                    { new TextIndex(-1), startTime + 0 },
-                    { new TextIndex(0), startTime + 500 },
-                    { new TextIndex(1), startTime + 600 },
-                    { new TextIndex(2), startTime + 1000 },
-                    { new TextIndex(3), startTime + 1500 },
-                    { new TextIndex(4), startTime + 2000 },
-                    { new TextIndex(8), startTime + 2500 },
-                };
-            });
-
-            AddStep("Only one time-tag", () =>
-            {
-                var startTime = getStartTime();
-
-                karaokeSpriteText.Clock = manualTime ? new FramedClock(manualClock) : Clock;
-                karaokeSpriteText.TimeTags = new Dictionary<TextIndex, double>
-                {
-                    { new TextIndex(0), startTime + 500 },
-                };
-            });
-
-            AddStep("None time-tag", () =>
-            {
-                karaokeSpriteText.Clock = manualTime ? new FramedClock(manualClock) : Clock;
-                karaokeSpriteText.TimeTags = null;
             });
 
             double getStartTime()
