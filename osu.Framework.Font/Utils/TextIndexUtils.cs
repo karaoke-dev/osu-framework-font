90f0e55bd421e6f7dfb73e4a79cbca5f2cacc557
andy840119 <andy840119@gmail.com>
2022-05-07 12:41:52 +0800

    Add all the utils that needed in this PR.

--- osu.Framework.Font/Utils/TextIndexUtils.cs
+++ osu.Framework.Font/Utils/TextIndexUtils.cs
@@ -12,5 +12,32 @@ namespace osu.Framework.Utils
         {
             return new TextIndex(Math.Clamp(value.Index, minValue, maxValue), value.State);
         }
+
+        public static int ToStringIndex(TextIndex index)
+        {
+            if (index.State == TextIndex.IndexState.Start)
+                return index.Index;
+
+            return index.Index + 1;
+        }
+
+        public static TextIndex.IndexState ReverseState(TextIndex.IndexState state)
+        {
+            return state == TextIndex.IndexState.Start ? TextIndex.IndexState.End : TextIndex.IndexState.Start;
+        }
+
+        public static TextIndex GetPreviousIndex(TextIndex originIndex)
+        {
+            int previousIndex = ToStringIndex(originIndex) - 1;
+            var previousState = ReverseState(originIndex.State);
+            return new TextIndex(previousIndex, previousState);
+        }
+
+        public static TextIndex GetNextIndex(TextIndex originIndex)
+        {
+            int nextIndex = ToStringIndex(originIndex);
+            var nextState = ReverseState(originIndex.State);
+            return new TextIndex(nextIndex, nextState);
+        }
     }
 }
