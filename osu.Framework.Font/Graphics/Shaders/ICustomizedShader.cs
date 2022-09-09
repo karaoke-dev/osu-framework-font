0cb4f8d00dc2d56768a9592f1d0694b03b8a75dc
andy840119 <andy840119@gmail.com>
2022-09-09 10:09:45 +0800

    Should not inherit the `IShader` anymore because it has internal method need to implement.

--- osu.Framework.Font/Graphics/Shaders/ICustomizedShader.cs
+++ osu.Framework.Font/Graphics/Shaders/ICustomizedShader.cs
@@ -1,10 +1,40 @@
 ﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+
 namespace osu.Framework.Graphics.Shaders
 {
-    public interface ICustomizedShader : IShader
+    public interface ICustomizedShader
     {
         void ApplyValue();
+
+        /// <summary>
+        /// Binds this shader to be used for rendering.
+        /// </summary>
+        void Bind();
+
+        /// <summary>
+        /// Unbinds this shader.
+        /// </summary>
+        void Unbind();
+
+        /// <summary>
+        /// Whether this shader is ready for use.
+        /// </summary>
+        bool IsLoaded { get; }
+
+        /// <summary>
+        /// Whether this shader is currently bound.
+        /// </summary>
+        bool IsBound { get; }
+
+        /// <summary>
+        /// Retrieves a uniform from the shader.
+        /// </summary>
+        /// <param name="name">The name of the uniform.</param>
+        /// <returns>The retrieved uniform.</returns>
+        Uniform<T> GetUniform<T>(string name)
+            where T : unmanaged, IEquatable<T>;
     }
 }
