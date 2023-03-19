6c094528de12da13dd2f1569b53e9f0a5aaca6d1
andy840119 <andy840119@gmail.com>
2023-03-19 10:00:58 +0800

    Fix the pixel shader.
    Co-authored-by: WebFreak001 <gh@webfreak.org>

--- osu.Framework.Font/Graphics/Shaders/PixelShader.cs
+++ osu.Framework.Font/Graphics/Shaders/PixelShader.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Runtime.InteropServices;
 using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Shaders.Types;
 using osuTK;
 
 namespace osu.Framework.Graphics.Shaders;
@@ -12,9 +14,21 @@ public class PixelShader : InternalShader, IHasTextureSize, IHasInflationPercent
 
     public Vector2 Size { get; set; } = new(5);
 
+    private IUniformBuffer<PixelParameters>? pixelParametersBuffer;
+
     public override void ApplyValue(IRenderer renderer)
     {
-        var size = Size;
-        GetUniform<Vector2>(@"g_Size").UpdateValue(ref size);
+        pixelParametersBuffer ??= renderer.CreateUniformBuffer<PixelParameters>();
+
+        pixelParametersBuffer.Data = new PixelParameters { Size = Size };
+
+        BindUniformBlock("m_PixelParameters", pixelParametersBuffer);
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    private record struct PixelParameters
+    {
+        public UniformVector2 Size;
+        private readonly UniformPadding8 pad1;
     }
 }
