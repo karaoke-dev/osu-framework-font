a85f0d4b69d0bf08988c7da114f1ee1b79b3b250
andy840119 <andy840119@gmail.com>
2023-03-25 19:57:09 +0800

    Write extension for able to convert the vector2 to the `UniformVector2` with right direction.

---
+++ osu.Framework.Font/Graphics/Extensions/RendererExtension.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Shaders.Types;
+using osuTK;
+
+namespace osu.Framework.Graphics.Extensions;
+
+public static class RendererExtension
+{
+    /// <summary>
+    /// Convert the vector2 to shader vector2.
+    /// Because might have different origin of uv if using different renderer.
+    /// </summary>
+    /// <param name="renderer"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static UniformVector2 ToShaderVector2(this IRenderer renderer, Vector2 vector2)
+    {
+        return vector2 * new Vector2(-1, renderer.IsUvOriginTopLeft ? -1 : 1);
+    }
+}
