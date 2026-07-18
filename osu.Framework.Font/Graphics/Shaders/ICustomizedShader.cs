0b11f912c344c68ae94cf7d30fcf16a073c22c9c
andy840119 <andy840119@gmail.com>
2026-07-18 22:05:12 +0800

    Bump the osu-related group: ppy.osu.Framework to 2026.629.0, ppy.osu.Game.Resources to 2026.710.0
    IShader.GetUniform<T>() was removed upstream (already dead code since
    GlobalPropertyManager removal), so drop the matching passthrough from
    ICustomizedShader/CustomizedShader/StepShader.

--- osu.Framework.Font/Graphics/Shaders/ICustomizedShader.cs
+++ osu.Framework.Font/Graphics/Shaders/ICustomizedShader.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Graphics.Rendering;
 
 namespace osu.Framework.Graphics.Shaders;
@@ -29,14 +28,6 @@ public interface ICustomizedShader
     /// Whether this shader is currently bound.
     /// </summary>
     bool IsBound { get; }
-
-    /// <summary>
-    /// Retrieves a uniform from the shader.
-    /// </summary>
-    /// <param name="name">The name of the uniform.</param>
-    /// <returns>The retrieved uniform.</returns>
-    Uniform<T> GetUniform<T>(string name)
-        where T : unmanaged, IEquatable<T>;
 
     /// <summary>
     /// Binds an <see cref="IUniformBuffer"/> to a uniform block of the given name.
